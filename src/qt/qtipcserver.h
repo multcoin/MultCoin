a14572cf051f2b2ea31211fb5e0204fd4a7ded10
multcoin <48601449+multcoin@users.noreply.github.com>
2019-03-18 16:49:29 -0300

    Initial commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define MultCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "MultCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
